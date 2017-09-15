3447732c163d6480dd6a5ffb80b657b757e18f6d
Chetna Chaudhari <chetnachaudhari@gmail.com>
2017-09-15 12:10:49 +1200

    some progress

--- Parse.py
+++ Parse.py
@@ -1,11 +1,36 @@
 #!/usr/bin/env python
 
-import argparse, os
+import argparse, os, re
 
 def print_file_name(fname):
     print(fname)
 
-def processFile():
+
+def savevcard(out_file, card):
+    with open(out_file, 'a') as final:
+        final.write(card)
+
+def processCards(in_file, out_file):
+    card = ''
+    count = 0
+    with open(in_file) as f:
+        for line in f:
+            line = line.replace('\r\n', '').replace('\n', '').strip()
+            card = card + line + '\n'
+            if line == 'END:VCARD':
+                if valid:
+                    savevcard(out_file, card)
+                card = ''
+                count += 1
+                print("Total contacts parsed : {}".format(count))
+            elif re.search('^FN:', line):
+                fullname = line[3:-1]
+                if fullname != '':
+                    valid = 1
+                else:
+                    valid = 0
+
+def processArguments():
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--input', help="Input File Name", required=True)
     parser.add_argument('-o', '--output', help="Output File Name", required=True)
@@ -13,11 +38,16 @@ def processFile():
     in_file = args.input
     out_file = args.output
 
+    if not os.path.isfile(in_file):
+        print("File does not exist")
+        return
 
-    
+    processCards(in_file, out_file)
+
+
     print_file_name(in_file)
     print_file_name(out_file)
 
 
 if __name__ == '__main__':
-    processFile()
+    processArguments()
